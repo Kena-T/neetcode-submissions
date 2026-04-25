2a3facd83526832111922a7154cf3c2262f9d225
Kena Tilahun <74115103+Kena-T@users.noreply.github.com>
2026-04-25 03:14:20 +0000

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -0,0 +1,20 @@
+import math
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # better algorithm O()
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
+
+        # brute force O(n)
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        return -1
